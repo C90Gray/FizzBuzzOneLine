3f3bcc27835a712cf97465c68bc41a961a30a8af
C90Gray <cmusic.gray@gmail.com>
2018-06-09 15:02:27 -0400

    first commit

---
+++ FizzBuzz_One_Line/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_One_Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose a whole number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine((x % 15==0) ? "FizzBuzz" : ((x % 3==0) ? "Fizz" : ((x % 5==0) ? "Buzz": x.ToString())));
+        }
+    }
+}
